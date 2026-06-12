790a90f5d6269a79020f3a1a57862eb2e72128f1
Harini S <harinisankaranarayanan06@gmail.com>
2026-06-12 21:12:51 +0530

    fix: syntax error in cors origins

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 const allowedOrigins = [
   'http://localhost:5173',
   'https://future-fs-02-flame-six.vercel.app',
-  'https://future-fs-02-mlg5rt4xd-harini-s-projects12.vercel.app'
+  'https://future-fs-02-mlg5rt4xd-harini-s-projects12.vercel.app',
   'https://future-fs-02-d1yz2bbgb-harini-s-projects12.vercel.app'
 ];
 
